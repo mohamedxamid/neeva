7bacb2421b01b5a35e29a9c073567386bd7e807d
Muhammad Xamidov <muhammadxamidov001@gmail.com>
2023-12-16 17:54:45 +0300

    Add new technologies, webpack, axios, json-server

--- js/main.js
+++ js/main.js
@@ -1,14 +1,50 @@
-const modifiers = {
-    siteHeaderActive: 'site-header--active',
-    sitenavTogglerActive: 'sitenav-toggler--active'
-};
+"use strict"
 
-const elSiteHeader = document.querySelector('.site-header');
-const elSitenavToggler = elSiteHeader.querySelector('.js-sitenav-toggler');
+import axios from "axios";
 
-if (elSitenavToggler) {
-    elSitenavToggler.addEventListener('click', function(evt) {
-        elSiteHeader.classList.toggle(modifiers.siteHeaderActive);
-        elSitenavToggler.classList.toggle(modifiers.sitenavTogglerActive);
+window.addEventListener('DOMContentLoaded', () => {
+    const modifiers = {
+        siteHeaderActive: 'site-header--active',
+        sitenavTogglerActive: 'sitenav-toggler--active'
+    };
+    
+    const elSiteHeader = document.querySelector('.site-header');
+    const elSitenavToggler = elSiteHeader.querySelector('.js-sitenav-toggler');
+    
+    if (elSitenavToggler) {
+        elSitenavToggler.addEventListener('click', function() {
+            elSiteHeader.classList.toggle(modifiers.siteHeaderActive);
+            elSitenavToggler.classList.toggle(modifiers.sitenavTogglerActive);
+        })
+    }
+    
+    const elsForm = document.querySelectorAll('form')
+    console.log(elsForm);
+    
+    elsForm.forEach(elForm => {
+        elForm.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            
+            const form = new FormData(evt.target);
+            const json = JSON.stringify(Object.fromEntries(form.entries()))
+
+            postData('http://localhost:3000/requests', json)
+            .then(data => console.log(data.data))
+            .catch(err => {
+                console.log(err.message);
+            })
+            .finally(() => {
+                evt.target.reset();
+            })
+        })
     })
-}
+    
+    
+    async function postData(url, data) {
+        return await axios.post(url, data, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+    }
+})
